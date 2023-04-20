dc198238529e72c9a5b4fe68c0dbf99f889def12
Sufiyan Razaq <119070430+SufiyanRazaq@users.noreply.github.com>
2023-04-21 01:51:43 +0500

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Programming\\FlutterSDK\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Programming\\Sufiyan projects\\TourApp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Programming\\FlutterSDK\\flutter"
+  "PROJECT_DIR=E:\\Programming\\Sufiyan projects\\TourApp"
+  "FLUTTER_ROOT=E:\\Programming\\FlutterSDK\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Programming\\Sufiyan projects\\TourApp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Programming\\Sufiyan projects\\TourApp"
+  "FLUTTER_TARGET=E:\\Programming\\Sufiyan projects\\TourApp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Programming\\Sufiyan projects\\TourApp\\.dart_tool\\package_config.json"
+)
